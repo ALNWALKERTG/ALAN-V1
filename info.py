a476a66ea7e15020f4887545bcf7b8257a61b973
ALNWALKERTG <166110788+ALNWALKERTG@users.noreply.github.com>
2024-09-03 21:35:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,7 +16,7 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '7679954')
+API_ID = int(environ.get('API_ID', '7679954'))
 API_HASH = environ.get('API_HASH', '4af51af8f1a8b06ca2b076370ba93fba')
 BOT_TOKEN = environ.get('BOT_TOKEN', '7256446411:AAHlug2MKleP0mx8sKucF9kUHd8XzxJfr78')
 
