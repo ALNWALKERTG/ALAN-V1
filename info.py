0ccfe00423bec317d7ab90b1183c4734e182b3a7
ALNWALKERTG <166110788+ALNWALKERTG@users.noreply.github.com>
2024-09-08 20:44:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,7 +29,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://envs.sh/bt3.jpg")
 # Admins, Channels & Users
 OWNER_ID = environ.get('OWNER_ID', '2144812475')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '2144812475 7040444713').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001911016310').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002180209193 -1001510645296 -1001651803539').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_grp = environ.get('AUTH_GROUP')
