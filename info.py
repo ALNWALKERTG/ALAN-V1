3717bbca651086d2ec2c9f0c05c7daec87529324
ʟᴇᴏ ᠰ ᴛɢ <ilashadam039@gmail.com>
2024-11-03 21:29:05 +0400

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7256446411:AAFzsdyJb0x7Jqsw1GISfKkSIC1QMyq
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS', 'https://envs.sh/bFL.jpg https://envs.sh/btj.jpg https://envs.sh/bt_.jpg')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://envs.sh/btz.jpg")
+NOR_IMG = environ.get("NOR_IMG", "https://envs.sh/_-v.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://envs.sh/bt3.jpg")
 # Admins, Channels & Users
 OWNER_ID = environ.get('OWNER_ID', '2144812475')
