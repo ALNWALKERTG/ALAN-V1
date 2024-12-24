0c03f2264fc18d51222bbd1b0ef533acc399d6b1
hakhddhujsjdyw <manafalthaf01@gmail.com>
2024-12-25 00:47:08 +0530

    Update id.py

--- plugins/id.py
+++ plugins/id.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters, enums
 import logging
 from Script import script
 from pyrogram.errors import ChatAdminRequired
-from database.ia_filterdb.py import Media2, Media3, Media4, Media5, get_file_details, get_search_results, get_bad_files, db as clientDB, db2 as clientDB2, db3 as clientDB3, db4 as clientDB4, db5 as clientDB5
+from database.ia_filterdb import Media2, Media3, Media4, Media5, get_file_details, get_search_results, get_bad_files, db as clientDB, db2 as clientDB2, db3 as clientDB3, db4 as clientDB4, db5 as clientDB5
 from info import ADMINS, LOG_CHANNEL
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
